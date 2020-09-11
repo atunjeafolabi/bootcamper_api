e24c7cc61151c337b44c3f29fb77fa9fb4b3f900
Atunje <atunje_afolabi@yahoo.com>
2020-09-11 08:39:14 +0100

    Added mongoose slugify middleware

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema(
     {
@@ -99,5 +100,12 @@ const BootcampSchema = new mongoose.Schema(
         }
     }
 );
+
+// Create bootcamp slug from the name.
+// We don't use Arrow function because of "this" keyword
+BootcampSchema.pre('save', function(next){
+    this.slug = slugify(this.name, {lower: true});
+    next();
+})
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
