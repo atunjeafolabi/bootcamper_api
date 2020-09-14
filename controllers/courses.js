f55378532c8806691836ecd7e105fdd4e31e6979
Atunje <atunje_afolabi@yahoo.com>
2020-09-14 10:05:11 +0100

    Create a course for a bootcamp

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const Course = require('../models/Course');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Get courses
 // @route   GET /api/v1/courses
@@ -23,4 +24,42 @@ exports.getCourses = asyncHandler(async(req, res, next) => {
         count: courses.length,
         data: courses
     });
-})
+});
+
+// @desc    Get  single course
+// @route   GET /api/v1/courses/:id
+// @access  Public
+exports.getCourse = asyncHandler(async(req, res, next) => {
+    
+    const course = await Course.findById(req.params.id).populate({path: 'bootcamp', select: 'name description'});
+
+    if(!course){
+        return next(new ErrorResponse(`No course found with the id of ${req.params.id}`));
+    }
+
+    res.status(200).json({
+        success: true,
+        data: course
+    });
+});
+
+// @desc    Add a course
+// @route   POST /api/v1/bootcamps/:bootcampId/courses
+// @access  Private
+exports.createCourse = asyncHandler(async(req, res, next) => {
+    
+    req.body.bootcamp = req.params.bootcampId;
+
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+    if(!bootcamp){
+        return next(new ErrorResponse(`No bootcamp found with the id of ${req.params.id}`), 404);
+    }
+
+    const course = await Course.create(req.body);
+    
+    res.status(201).json({
+        success: true,
+        data: course
+    });
+});
--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
 const {
-    getCourses
+    getCourses,
+    getCourse,
+    createCourse
 } = require('../controllers/courses');
 
 router.route('/')
-    .get(getCourses);
+    .get(getCourses)
+    .post(createCourse);
+
+router.route('/:id')
+    .get(getCourse);
 
 module.exports = router;
