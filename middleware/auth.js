45c85ecc4b4a21c3a89ac58c530e14b85b791ccb
Atunje <atunje_afolabi@yahoo.com>
2020-10-01 07:29:35 +0100

    Add Logout

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,8 +11,13 @@ exports.protect = asyncHandler(async (req, res, next) => {
         req.headers.authorization &&
         req.headers.authorization.startsWith('Bearer')
     ) {
+        // Set token from header
         token = req.headers.authorization.split(' ')[1];
-    }
+    } 
+    // else if (req.cookies.token) {
+    //     // Set token from cookie
+    //     token = req.cookies.token;
+    // }
 
     // Make sure token exists
     if(!token){
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const {register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword} = require('../controllers/auth');
+const {
+    register,
+    login,
+    getMe,
+    forgotPassword,
+    resetPassword,
+    updateDetails,
+    updatePassword,
+    logout
+} = require('../controllers/auth');
 const router = express.Router();
 const {protect, authorize} = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
 router.get('/me', protect, getMe);
 router.post('/forgotPassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
